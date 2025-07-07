1e27cfd9c0cdd69469be5043adba89e8d7557d18
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-07-07 14:59:44 +0300

    Java Advanced Reminders

---
+++ src/main/java/com/ayhanunlu/tutorials/JavaTutorials.java
@@ -0,0 +1,70 @@
+package com.ayhanunlu.tutorials;
+
+import java.util.Arrays;
+
+public class JavaTutorials {
+    public static void main(String[] args) {
+        /*Arrays.sort()
+        this.
+        super
+        break continue return
+        optional
+        primitive type
+        wrapper class
+        Random
+        Math
+        %s %d %f printf()
+        final
+        static
+        decoder
+        encoder
+        instanceOf
+        public private protected
+        String
+        StringBuilder
+        StringBuffer
+        StringTokenizer
+        Enum
+        Multithread
+
+        S.O.L.I.D
+        KISS
+        DRY
+        MONAD
+        List Set Map
+        List: LinkedList ArrayList
+        Generic <>
+        Iterator
+        OOP
+        interface
+        abstract
+        inheritance
+
+        UML - + __
+        class
+        POJO
+        Bean
+        Serialisation
+        IO
+        JAR WAR
+        server
+
+
+        constructor
+        exception
+        error
+
+        Java 8 : stream, map,filter,sort
+        Optional
+        Lambda Expression
+        Method References ::
+
+        D.P.--design pattern--
+
+*/
+
+
+    }
+
+
+}
