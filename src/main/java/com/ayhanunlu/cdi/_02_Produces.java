060781dcff2e7298f0e4f761c2ecd62507bebf71
Ayhan Unlu <103220953+ayhan-unlu@users.noreply.github.com>
2025-07-14 16:27:02 +0300

    @Produces

---
+++ src/main/java/com/ayhanunlu/cdi/_02_Produces.java
@@ -0,0 +1,22 @@
+package com.ayhanunlu.cdi;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named(value = "producetuto")
+@ApplicationScoped
+public class _02_Produces {
+@Produces//Produces
+    public List<String> getList(){
+        List<String> list = new ArrayList<>();
+        list.add("html5");
+        list.add("css3");
+        list.add("js");
+        list.add("react");
+        list.add("angular");
+        return list;
+    }
+}
